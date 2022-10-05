9f8a3f5179bbf9d3487ee0598f4824e7d16d168d
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-10-05 17:59:03 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,152 +1,179 @@
-
 //creating card Array
-    const cardArray = [
-        {
-            name: 'logo',
-            img: 'img/c1.jpg'
-        },
-        {
-            name: 'logo',
-            img: 'img/c1.jpg'
-        },
-        {
-            name: 'mainCharacter',
-            img: 'img/c2.jpg'
-        },
-        {
-            name: 'mainCharacter',
-            img: 'img/c2.jpg'
-        },
-        {
-            name: 'DJ',
-            img: 'img/c3.jpg'
-        },
-        {
-            name: 'DJ',
-            img: 'img/c3.jpg'
-        },
-        {
-            name: 'bike',
-            img: 'img/c4.jpg'
-        },
-        {
-            name: 'bike',
-            img: 'img/c4.jpg'
-        },
-        {
-            name: 'cyPunk',
-            img: 'img/c5.jpg'
-        },
-        {
-            name: 'cyPunk',
-            img: 'img/c5.jpg'
-        },
-        {
-            name: 'team',
-            img: 'img/c6.jpg'
-        },
-        {
-            name: 'team',
-            img: 'img/c6.jpg'
-        }
-    ]        
+const cardArray = [
+    {
+        name: 'logo',
+        img: 'img/c1.jpg'
+    },
+    {
+        name: 'logo',
+        img: 'img/c1.jpg'
+    },
+    {
+        name: 'mainCharacter',
+        img: 'img/c2.jpg'
+    },
+    {
+        name: 'mainCharacter',
+        img: 'img/c2.jpg'
+    },
+    {
+        name: 'DJ',
+        img: 'img/c3.jpg'
+    },
+    {
+        name: 'DJ',
+        img: 'img/c3.jpg'
+    },
+    {
+        name: 'bike',
+        img: 'img/c4.jpg'
+    },
+    {
+        name: 'bike',
+        img: 'img/c4.jpg'
+    },
+    {
+        name: 'cyPunk',
+        img: 'img/c5.jpg'
+    },
+    {
+        name: 'cyPunk',
+        img: 'img/c5.jpg'
+    },
+    {
+        name: 'team',
+        img: 'img/c6.jpg'
+    },
+    {
+        name: 'team',
+        img: 'img/c6.jpg'
+    }
+]      
+
+
+
+
 
 //sorting card array so each time
 // user refresh page images will go in random positions
+
 cardArray.sort(()=> 0.5 - Math.random());
 
 const grid = document.querySelector('.grid');
 const resultDisplay = document.querySelector('#result');
-var cardsChosen =[];
- var cardsChosenId =[];
- var cardsWon =[];
 
+var cardsChosen = [];
+var cardsChosenId = [];
+var cardsWon = [];
 
-//create board of cards with images from array
+//create board of cards func with images from array
 function createBoard(){
 
-    for(let i =0; i< cardArray.length; i++){
-        var card = document.createElement('img');
-        card.setAttribute('src', 'img/blank.jpg');
-        card.setAttribute('data-id', i);
-        card.addEventListener('click', flipCard)
-        grid.appendChild(card);
-    }
+ //for loop with cardArray
+
+for(let i = 0; i< cardArray.length; i++){
+ var card = document.createElement('img');
+ card.setAttribute('src', 'img/blank.jpg');
+ card.setAttribute('datai-id', i);
+ card.addEventListener('click', flipCard);
+ grid.appendChild(card);
+
 }
 
+ //addeventlisthener on flipcard
+
+
+
+//create const card that will createElement img which will become board
+//set attribute that will hide images
+
+
+ //setting attr data id to i in for loop
+
+
+ //appending card to grid
+
+
+//}
+}
+
+function checkForMatches(){
+
+ var cards = document.querySelectorAll('img');
+ const optionOne = cardsChosenId[0];
+ const optionTwo = cardsChosenId[1];
+if(cardsChosen[0] === cardsChosen[1]){
+ alert('you found a match');
+ cards[optionOne].setAttribute('src', 'img/white.png');
+ cards[optionTwo].setAttribute('src', 'img//white.png');
+ cardsWon.push(cardsChosen);
+}else{
+
+ cards[optionOne].setAttribute('src', 'img/blank.jpg');
+ cards[optionTwo].setAttribute('src', 'img/blank.jpg');
+ alert('sorry try again');
+}
+cardsChosen = [];
+cardsChosenId = [];
+
+resultDisplay.textContent = cardsWon.length;
+if(cardsWon.length === cardArray.length/2){
+    resultDisplay.textContent = 'congrats';
+}
+}
 //Check for matches
-function checkforMatch(){
-//setting if two cards are the same, 0 and 1
+ //create cards variable
+ //creating two const option id 1 and 2
+
+//setting if two chosen cards are the same, if yes we will replace images to white blank
+// we then set cards  to option one and two wich are images we created for board
 //the alert you found match and the cards will appear to be white
 //if not gradient blank appear
-    var cards = document.querySelectorAll('img');
-    const optionOneId = cardsChosenId[0];
-    const optionTwoId= cardsChosenId[1];
+//we then push what we set in if statements to cardswon array t
 
-    if(cardsChosen[0] === cardsChosen[1]){
-        alert('you found a match');
-        cards[optionOneId].setAttribute('src', 'img/white.png');
-        cards[optionTwoId].setAttribute('src', 'img/white.png');
-        cardsWon.push(cardsChosen);
- 
-        }else{
-        cards[optionOneId].setAttribute('src', 'img/blank.jpg');
-        cards[optionTwoId].setAttribute('src', 'img/blank.jpg');
-        alert('sorry try again');
-    }
-    cardsChosen=[];
-    cardsChosenId=[];
-    //if you predict all the images the textcontent will say congrats
-    resultDisplay.textContent = cardsWon.length;
-    if(cardsWon.length === cardArray.length/2){
-        resultDisplay.textContent = 'congrats you got all the matches correct';
-    }
+
+
+
+//if you predict all the images the textcontent will say congrats
+
+
+//}
+// }
+
+
+//flip your card func
+
+function flipCard(){
+
+
+ var cardId = this.getAttribute('data-id');
+
+ cardsChosen.push(cardArray[cardId].name);
+ cardsChosenId.push(cardId);
+
+ this.setAttribute('src', cardArray[cardId].img);
+ if(cardsChosen.length === 2){
+ setTimeout(checkForMatches, 500);
+}
 
 }
-//flip your card
-function flipCard(){
+createBoard();
+
+
+//    Element.setAttribute() Sets the value of an attribute on the specified element
+
+//    getAttribute() The getAttribute() method of the Element interface returns the value of a specified attribute on the element.
+
 
 //create var that will get attribute of data-id
 //then push card chosen and card chosen id
-        var cardId = this.getAttribute('data-id');
-
-        cardsChosen.push(cardArray[cardId].name);
-        cardsChosenId.push(cardId);
-
-
- 
-
-//then we will set attribute so data -id will appear as car img
-//then we will check if we pick two cards then it will check for match
-        this.setAttribute('src', cardArray[cardId].img);
-        if(cardsChosen.length === 2 ){
-            setTimeout(checkforMatch, 500)
-        }
-        
-
-  
-
-}
-
-
-//createboard
-createBoard();
-    
 
 
 
+//then we will set attribute so data -id will appear as card id img
+//then we will check if cardsChosen.length === 2 then it will check for match
+//setting timeout check with matches, 500
+
+//}
 
 
-
-
-
-
-// const clickMe = document.querySelector('.listImg img');
-// console.log(clickMe);
-
-
-// clickMe.addEventListener('click', ()=>{
-
-
-
-// })
